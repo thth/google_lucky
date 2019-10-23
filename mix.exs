58ff3643349a53802be17582ee148741cd4db9a2
thth <ththfh@gmail.com>
2019-10-22 21:41:29 -0700

    fix version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule GoogleLucky.MixProject do
     [
       app: :google_lucky,
       version: "0.1.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
@@ -34,7 +34,7 @@ defmodule GoogleLucky.MixProject do
     [
       {:phoenix, "~> 1.4.4"},
       {:phoenix_pubsub, "~> 1.1"},
-      {:gettext, "~> 0.11"},
+      {:gettext, "~> 0.11.0"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:httpoison, "~> 1.6"},
