5585e184b60e09e8aee29134d912c14d68eb81fe
thth <ththfh@gmail.com>
2019-12-21 15:20:12 -0800

    fix search to use link instead of formattedURL from api response

--- lib/google_lucky/search.ex
+++ lib/google_lucky/search.ex
@@ -1,20 +1,33 @@
 defmodule GoogleLucky.Search do
   def search(params) do
     # maybe in future add option in config to add additional params per API
-    # 
     url = "https://www.googleapis.com/customsearch/v1"
+
     params =
       Application.fetch_env!(:google_lucky, GoogleLucky.Search)
       |> Map.merge(params)
       |> Map.put("num", "1")
+
     case HTTPoison.get(url, [], params: params) do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        %{"items" => [%{"formattedUrl" => url} | _]} = Jason.decode!(body)
-        {:ok, url}
+        link = get_url_from_response_body(body)
+        {:ok, link}
       {:ok, %HTTPoison.Response{body: body}} ->
         {:error, body}
       {:error, error} ->
         {:error, Kernel.inspect(error)}
     end
+  end
+
+  defp get_url_from_response_body(body) do
+    %{
+      "items" => [
+        %{
+          "link" => link
+        }
+      ]
+    } = Jason.decode!(body)
+
+    link
   end
 end
